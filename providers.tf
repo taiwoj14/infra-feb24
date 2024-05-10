f83b722dabd9ec5bc68e04dd40d42422e5c082c2
taiwoj14 <129912914+taiwoj14@users.noreply.github.com>
2024-05-10 15:17:07 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "tay-terraform-state"
+    key = "global/s3/terraform.tfstate"
+    dynamodb_table = "terraform-state"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
 
 
 # Configure the AWS Provider
